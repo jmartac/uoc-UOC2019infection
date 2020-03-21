d5ad29bb7162048f27cf2092db7105eec94fbd4b
Javi <javiermartac@gmail.com>
2020-03-21 13:36:11 +0000

    Final version
    all EX checked as OK; EX 1, 2 and 3 errors fixed

--- UOCinfectiousAgent/src/infection.c
+++ UOCinfectiousAgent/src/infection.c
@@ -7,96 +7,306 @@
 #include <stdio.h>
 
 // Initialize the Infection structure
-tError infection_init(tInfection* object, tInfectiousAgent* infectiousAgent, const char* country,  tDate* date, int cases, int deaths){
-	// PR1_EX4
+tError
+infection_init(tInfection *object, tInfectiousAgent *infectiousAgent, const char *country, tDate *date, int cases,
+               int deaths) {
 
-    return ERR_NOT_IMPLEMENTED;
+    // PR1_EX4
+    assert(object != NULL);
+    assert(infectiousAgent != NULL);
+    assert(date != NULL);
+
+    // memory allocation
+    object->infectiousAgent = (tInfectiousAgent *) calloc(1, sizeof(tInfectiousAgent));
+    object->country = (char *) malloc(strlen(country) + 1 * sizeof(char));
+    object->date = (tDate *) malloc(sizeof(tDate));
+
+    // checking for memory errors
+    if (object->infectiousAgent == NULL ||
+        object->country == NULL ||
+        object->date == NULL) {
+        return ERR_MEMORY_ERROR;
+    }
+
+    // deep copy of values
+    strcpy(object->country, country);
+    object->date->day = date->day;
+    object->date->month = date->month;
+    object->date->year = date->year;
+    object->cases = cases;
+    object->deaths = deaths;
+
+    // abort if the copy of infectiousAgent fails (if not, returns OK)
+    return infectiousAgent_cpy(object->infectiousAgent, infectiousAgent);
 }
 
 // Remove the memory used by Infection structure
-void infection_free(tInfection* object){
-	// PR1_EX4
+void infection_free(tInfection *object) {
+
+    // PR1_EX4
+
+    assert(object != NULL);
+
+    if (object->infectiousAgent != NULL) {
+        infectiousAgent_free(object->infectiousAgent);
+        object->infectiousAgent = NULL;
+    }
+    if (object->country != NULL) {
+        free(object->country);
+        object->country = NULL;
+    }
+    if (object->date != NULL) {
+        free(object->date);
+        object->date = NULL;
+    }
+    object->cases = 0;
+    object->deaths = 0;
 }
 
 // Update cases and deaths of an Infection
-void infection_update(tInfection* infection, int cases, int deaths){
-	// PR1_EX4
+void infection_update(tInfection *infection, int cases, int deaths) {
+
+    // PR1_EX4
+    assert(infection != NULL);
+
+    infection->cases += cases;
+    infection->deaths += deaths;
 }
 
 // Compare two infections
-bool infection_equals(tInfection* infection1, tInfection* infection2) {    
-	// PR1_EX4
+bool infection_equals(tInfection *infection1, tInfection *infection2) {
 
-    return false;   
+    // PR1_EX4
+    assert(infection1 != NULL);
+    assert(infection2 != NULL);
+
+    if (strcmp(infection1->infectiousAgent->name, infection2->infectiousAgent->name) != 0) {
+        return false;
+    }
+    if (strcmp(infection1->country, infection2->country) != 0) {
+        return false;
+    }
+
+    return true;
 }
 
 // Get the size of the table
-unsigned int infectionTable_size(tInfectionTable* table){
-    // PR1_EX4
+unsigned int infectionTable_size(tInfectionTable *table) {
 
-    return ERR_NOT_IMPLEMENTED;
+    // PR1_EX4
+    assert(table != NULL);
+
+    return table->size;
 }
 
 // Initialize the Table of infections
-void infectionTable_init(tInfectionTable* table){
-	// PR1_EX4
+void infectionTable_init(tInfectionTable *table) {
+
+    // PR1_EX4
+    assert(table != NULL);
+
+    table->size = 0;
+    table->elements = NULL;
 }
 
 // Remove the memory used by InfectionTable structure
-void infectionTable_free(tInfectionTable* object){
-	// PR1_EX4
+void infectionTable_free(tInfectionTable *object) {
+
+    // PR1_EX4
+    assert(object != NULL);
+
+    if (object->elements != NULL) {
+        free(object->elements);
+        object->elements = NULL;
+    }
+    object->size = 0;
 }
 
 // Add a new Infection to the table
-tError infectionTable_add(tInfectionTable* table, tInfection* infection){
-	// PR1_EX4
+tError infectionTable_add(tInfectionTable *table, tInfection *infection) {
 
-    return ERR_NOT_IMPLEMENTED;
+    // PR1_EX4
+    assert(table != NULL);
+    assert(infection != NULL);
+
+    if (infectionTable_find(table, infection->infectiousAgent->name, infection->country))
+        return ERR_DUPLICATED;
+
+    table->size++;
+    if (table->size == 0) {
+        // Empty table
+        table->elements = (tInfection *) malloc(table->size * sizeof(tInfection));
+    } else {
+        // table with 1 or more elements
+        table->elements = (tInfection *) realloc(table->elements, table->size * sizeof(tInfection));
+    }
+
+    if (table->elements == NULL) {
+        // Error allocating or reallocating the memory
+        return ERR_MEMORY_ERROR;
+    }
+
+    // Adds a new initialized infection with the given params in the last location of table->elements
+    return infection_init(&(table->elements[table->size - 1]), infection->infectiousAgent, infection->country,
+                          infection->date, infection->cases, infection->deaths);
 }
 
 // Get Infection by Infection and country name
-tInfection* infectionTable_find(tInfectionTable* table, const char* infectiousAgentName, const char* country){
-	// PR1_EX4
+tInfection *infectionTable_find(tInfectionTable *table, const char *infectiousAgentName, const char *country) {
 
+    // PR1_EX4
+    assert(table != NULL);
+
+    int i;
+
+    for (i = 0; i < table->size; i++) {
+        if (strcmp(table->elements[i].infectiousAgent->name, infectiousAgentName) == 0 &&
+            strcmp(table->elements[i].country, country) == 0)
+            return &(table->elements[i]);
+    }
+
+    // The element has not been found
     return NULL;
 }
 
 // Compare two Table of infections
-bool infectionTable_equals(tInfectionTable* infectionTable1, tInfectionTable* infectionTable2){
-	// PR1_EX4
+bool infectionTable_equals(tInfectionTable *infectionTable1, tInfectionTable *infectionTable2) {
 
-    return false;    
+    // PR1_EX4
+    assert(infectionTable1 != NULL);
+    assert(infectionTable2 != NULL);
+
+    unsigned int i, j;
+    bool found;
+
+    if (infectionTable1->size != infectionTable2->size)
+        // having different sizes is considered as not equal
+        return false;
+
+    // Search over the table and return once we found the element.
+    for (i = 0; i < infectionTable1->size; i++) {
+        found = false;
+        for (j = 0; j < infectionTable2->size; j++) {
+            if (infection_equals(&infectionTable1->elements[i], &infectionTable2->elements[j])) {
+                found = true;
+                break;
+            }
+        }
+        if (!found)
+            // the second table doesn't have the [i] element of first table, so they're not equal
+            return false;
+    }
+
+    return true;
 }
 
 // Copy the data of a Infection to another Infection
-tError infection_cpy(tInfection* dst, tInfection* src){
-	// PR1_EX4
+tError infection_cpy(tInfection *dst, tInfection *src) {
 
-    return ERR_NOT_IMPLEMENTED;
+    // PR1_EX4
+    assert(dst != NULL);
+    assert(src != NULL);
+
+    infection_free(dst);
+
+    return infection_init(dst, src->infectiousAgent, src->country, src->date, src->cases, src->deaths);
 }
 
 // Remove a Infection from the table
-tError infectionTable_remove(tInfectionTable* table, tInfection* infection){
-	// PR1_EX4
+tError infectionTable_remove(tInfectionTable *table, tInfection *infection) {
 
-    return ERR_NOT_IMPLEMENTED;
+    // PR1_EX4
+    assert(table != NULL);
+    assert(infection != NULL);
+
+    int i;
+    bool found;
+
+    // It will relocate all items after the one to be removed to one less position
+    found = false;
+    for (i = 0; i < table->size; i++) {
+        if (found) {
+            // Copy element into its previous position
+            if (infection_cpy(&(table->elements[i - 1]), &(table->elements[i])) == ERR_MEMORY_ERROR)
+                // Error allocating or reallocating the memory
+                return ERR_MEMORY_ERROR;
+        } else if (infection_equals(&table->elements[i], infection)) {
+            // The current element is the element we want to remove. Set found flag to true to start element relocating.
+            found = true;
+        }
+    }
+
+    if (found) {
+        // Modify size and used memory for elements
+        table->size--;
+
+        if (table->size == 0) {
+            table->elements = NULL;
+        } else {
+            table->elements = (tInfection *) realloc(table->elements, table->size * sizeof(tInfection));
+
+            if (table->elements == NULL) {
+                // Error allocating or reallocating the memory
+                return ERR_MEMORY_ERROR;
+            }
+        }
+    } else {
+        // The element was not found in the table
+        return ERR_NOT_FOUND;
+    }
+
+    return OK;
 }
 
 // Given an infectious agent and a table of type tinfectionTable, 
 // it performs a search of the country with the largest infected population, 
 // offering us a pointer to it. In case of a tie, the country that is first on the list will be returned. 
 // In case of not finding the infectious agent in the list, it will return NULL.
-tInfection* infectionTable_getMaxInfection (tInfectionTable* table, const char* infectiousAgentName){
-	// PR1_EX4
+tInfection *infectionTable_getMaxInfection(tInfectionTable *table, const char *infectiousAgentName) {
 
-    return NULL;
+    // PR1_EX4
+    assert(table != NULL);
+
+    int i, max_cases_found;
+    tInfection *max_infection;
+
+    max_cases_found = -1;
+    max_infection = NULL;
+    for (i = 0; i < table->size; i++) {
+        if (strcmp(table->elements[i].infectiousAgent->name, infectiousAgentName) == 0) {
+            // only cases of infections with the same name as infectiousAgentName are considered
+            if ((table->elements[i].cases > max_cases_found) ||
+                (table->elements[i].cases == max_cases_found && table->elements[i].deaths > max_infection->deaths)) {
+                // max_infection will only change if the infection of the current iteration has more cases or if it has
+                // the same number of cases but more deaths. In case of a tie, the earlier will remain
+                max_cases_found = table->elements[i].cases;
+                max_infection = &table->elements[i];
+            }
+        }
+    }
+    return max_infection;
 }
 
 // Given an infectious agent and a tinfectionTable type table, 
 // calculate the mortality rate of an infectious agent worldwide, 
 // adding all the deceased and dividing it by the number of affected.
-float infectionTable_getMortalityRate (tInfectionTable* table, const char* infectiousAgentName){
-	// PR1_EX4
+float infectionTable_getMortalityRate(tInfectionTable *table, const char *infectiousAgentName) {
 
-    return 0.0;
+    // PR1_EX4
+    assert(table != NULL);
+
+    int i;
+    float total_cases, total_deaths;
+
+    total_cases = 0;
+    total_deaths = 0;
+    for (i = 0; i < table->size; i++) {
+        if (strcmp(table->elements[i].infectiousAgent->name, infectiousAgentName) == 0) {
+            // only cases and deaths of infections with the same name as infectiousAgentName are counted
+            total_cases += (float) table->elements[i].cases;
+            total_deaths += (float) table->elements[i].deaths;
+        }
+    }
+
+    return total_deaths / total_cases;
 }
--- UOCinfectiousAgent/src/infectiousAgent.c
+++ UOCinfectiousAgent/src/infectiousAgent.c
@@ -4,26 +4,38 @@
 #include "infectiousAgent.h"
 
 // Initialize the infectious agent structure
-tError infectiousAgent_init(tInfectiousAgent* object, char* name, float r0, char* medium, tDate* date, char* city, tReservoirTable* reservoirList) {
+tError infectiousAgent_init(tInfectiousAgent *object, char *name, float r0, char *medium, tDate *date, char *city,
+                            tReservoirTable *reservoirList) {
 
     // PR1_EX2
-    //TODO check if it must be the same address or a new one
-//    object->reservoirList = (tReservoirTable*) malloc(sizeof(tReservoirTable));
-//    object->reservoirList->elements = (tReservoir*) malloc(reservoirList->size * sizeof(tReservoir));
+    assert(object != NULL);
+    assert(reservoirList != NULL);
+    assert(date != NULL);
 
-    object->name = (char*) malloc(strlen(name)+1 * sizeof(char));
-    object->medium = (char*) malloc(strlen(medium)+1 * sizeof(char));
-    object->date = (tDate*) malloc(sizeof(tDate));
-    object->city = (char*) malloc(strlen(city)+1 * sizeof(char));
 
-    if( object->name == NULL ||
+    int i;
+    tReservoir *reservoir_aux;
+    tError result;
+
+    // memory allocation
+    object->name = (char *) malloc(strlen(name) + 1 * sizeof(char));
+    object->medium = (char *) malloc(strlen(medium) + 1 * sizeof(char));
+    object->date = (tDate *) malloc(sizeof(tDate));
+    object->city = (char *) malloc(strlen(city) + 1 * sizeof(char));
+    object->reservoirList = (tReservoirTable *) malloc(sizeof(tReservoirTable));
+    reservoirTable_init(object->reservoirList);
+
+    // checking for memory errors
+    if (object->name == NULL ||
         object->medium == NULL ||
         object->date == NULL ||
-        object->city == NULL)
+        object->city == NULL ||
+        object->reservoirList == NULL)
     {
         return ERR_MEMORY_ERROR;
     }
 
+    // deep copy of values
     strcpy(object->name, name);
     object->r0 = r0;
     strcpy(object->medium, medium);
@@ -31,95 +43,112 @@ tError infectiousAgent_init(tInfectiousAgent* object, char* name, float r0, char
     object->date->month = date->month;
     object->date->year = date->year;
     strcpy(object->city, city);
-    object->reservoirList = reservoirList;
+
+    // Initializes a copy of each reservoirList param, then adds it to object->objectList
+    // If there are any errors during this process, the method will abort the operation and return the appropriate error code
+    for (i = 0; i < reservoirList->size; i++) {
+        reservoir_aux = (tReservoir *) malloc(sizeof(tReservoir));
+        if (reservoir_aux == NULL)
+            return ERR_MEMORY_ERROR;
+
+        result = reservoir_init(reservoir_aux, reservoirList->elements[i].name, reservoirList->elements[i].species);
+        if (result == OK) {
+            result = reservoirTable_add(object->reservoirList, reservoir_aux);
+            if (result != OK)
+                return result;
+        } else
+            return result;
+        free(reservoir_aux);
+    }
 
     return OK;
 }
 
 // Remove the memory used by infectious agent structure
-void infectiousAgent_free(tInfectiousAgent* object) {
-    
-    // PR1_EX2
-    //TODO must change if the infectiousAgent_init has change the way iDate is init
+void infectiousAgent_free(tInfectiousAgent *object) {
 
+    // PR1_EX2
     assert(object != NULL);
 
-    if(object->name != NULL) {
+    if (object->name != NULL) {
         free(object->name);
         object->name = NULL;
     }
-    object->r0 = 0;
-    if(object->medium != NULL) {
+    if (object->medium != NULL) {
         free(object->medium);
         object->medium = NULL;
     }
-    if(object->date != NULL) {
+    if (object->date != NULL) {
         free(object->date);
         object->date = NULL;
     }
-    if(object->city != NULL) {
+    if (object->city != NULL) {
         free(object->city);
         object->city = NULL;
     }
-    if(object->reservoirList != NULL) {
+    if (object->reservoirList != NULL) {
         reservoirTable_free(object->reservoirList);
         object->reservoirList = NULL;
     }
+
+    object->r0 = 0;
 }
 
 // Get the reservoirs list of an infectious agent
-tReservoirTable* infectiousAgent_getReservoirs(tInfectiousAgent* object) {
-    
-	// PR1_EX2
+tReservoirTable *infectiousAgent_getReservoirs(tInfectiousAgent *object) {
 
-	return object->reservoirList;
+    // PR1_EX2
+    assert(object != NULL);
+
+    return object->reservoirList;
 }
 
 // Compare two infectious agent
-bool infectiousAgent_equals(tInfectiousAgent* infectiousAgent1, tInfectiousAgent* infectiousAgent2) {
-    
-    // PR1_EX2
-//todo remove this before last release
-    if (infectiousAgent1 == NULL || infectiousAgent2 == NULL)
-        return false;
+bool infectiousAgent_equals(tInfectiousAgent *infectiousAgent1, tInfectiousAgent *infectiousAgent2) {
 
-    if(strcmp(infectiousAgent1->name, infectiousAgent2->name) != 0) {
+    // PR1_EX2
+    assert(infectiousAgent1 != NULL);
+    assert(infectiousAgent2 != NULL);
+
+    if (strcmp(infectiousAgent1->name, infectiousAgent2->name) != 0) {
         return false;
     }
-    if(infectiousAgent1->r0 != infectiousAgent2->r0) {
+    if (infectiousAgent1->r0 != infectiousAgent2->r0) {
         return false;
     }
-    
+
     return true;
 }
 
 // Copy the data of a infectious agent to another infectious agent
-tError infectiousAgent_cpy(tInfectiousAgent* dest, tInfectiousAgent* src) {
-    
+tError infectiousAgent_cpy(tInfectiousAgent *dest, tInfectiousAgent *src) {
+
     // PR1_EX2
+    assert(dest != NULL);
+    assert(src != NULL);
 
     infectiousAgent_free(dest);
 
     return infectiousAgent_init(dest, src->name, src->r0, src->medium, src->date, src->city, src->reservoirList);
-	
-//    return OK;
 }
 
 // Initialize the Table of infectious agents
-void infectiousAgentTable_init(tInfectiousAgentTable* table) {
-	
+void infectiousAgentTable_init(tInfectiousAgentTable *table) {
+
     // PR1_EX3
+    assert(table != NULL);
 
     table->size = 0;
     table->elements = NULL;
 }
 
 // Remove the memory used by infectious agent table structure
-void infectiousAgentTable_free(tInfectiousAgentTable* object) {
-	
-    // PR1_EX3
+void infectiousAgentTable_free(tInfectiousAgentTable *object) {
 
-    if(object->elements != NULL) {
+    // PR1_EX3
+    assert(object != NULL);
+
+    if (object->elements != NULL) {
         free(object->elements);
         object->elements = NULL;
     }
@@ -127,122 +156,102 @@ void infectiousAgentTable_free(tInfectiousAgentTable* object) {
 }
 
 // Add a new infectious agent to the table
-tError infectiousAgentTable_add(tInfectiousAgentTable* table, tInfectiousAgent* infectiousAgent) {
-    
-    // PR1_EX3
+tError infectiousAgentTable_add(tInfectiousAgentTable *table, tInfectiousAgent *infectiousAgent) {
 
-    // Check if reservoirs already is on the table
+    // PR1_EX3
+    assert(table != NULL);
+    assert(infectiousAgent != NULL);
+
     if (infectiousAgentTable_find(table, infectiousAgent->name))
         return ERR_DUPLICATED;
 
-    // The first step is to allocate the required space. There are two methods to manage the memory (malloc and realloc). Malloc allows to allocate a new memory block, while realloc allows to modify an existing memory block.
-    if(table->size == 0) {
+    table->size++;
+    if (table->size == 0) {
         // Empty table
-
-        // Increase the number of elements of the table
-        table->size++;
-
-        // Since the table is empty, and we do not have any previous memory block, we have to use malloc. The amount of memory we need is the number of elements (will be 1) times the size of one element, which is computed by sizeof(type). In this case the type is tReservoir.
         table->elements = (tInfectiousAgent *) malloc(table->size * sizeof(tInfectiousAgent));
     } else {
-        // table with elements
-
-        // Increase the number of elements of the table
-        table->size++;
-
-        // Since the table is not empty, we already have a memory block. We need to modify the size of this block, using the realloc command. The amount of memory we need is the number of elements times the size of one element, which is computed by sizeof(type). In this case the type is tReservoir. We provide the previous block of memory.
-        table->elements = (tInfectiousAgent*) realloc(table->elements, table->size * sizeof(tInfectiousAgent));
+        // table with 1 or more elements
+        table->elements = (tInfectiousAgent *) realloc(table->elements, table->size * sizeof(tInfectiousAgent));
     }
 
-    // Check that the memory has been allocated
-    if(table->elements == NULL) {
+    if (table->elements == NULL) {
         // Error allocating or reallocating the memory
         return ERR_MEMORY_ERROR;
     }
 
-    // Once we have the block of memory, which is an array of tReservoir elements, we initialize the new element (which is the last one). The last element is " table->elements[table->size - 1] " (we start counting at 0)
-    return infectiousAgent_init(&(table->elements[table->size - 1]), infectiousAgent->name, infectiousAgent->r0, infectiousAgent->medium, infectiousAgent->date, infectiousAgent->city, infectiousAgent->reservoirList);
-	
-    return ERR_NOT_IMPLEMENTED;
-}                      
+    // Adds a new initialized infectiousAgent with the given params in the last location of table->elements
+    return infectiousAgent_init(&(table->elements[table->size - 1]), infectiousAgent->name, infectiousAgent->r0,
+                                infectiousAgent->medium, infectiousAgent->date, infectiousAgent->city,
+                                infectiousAgent->reservoirList);
+}
 
 // Remove a infectious agent from the table
-tError infectiousAgentTable_remove(tInfectiousAgentTable* table, tInfectiousAgent* infectiousAgent) {
-    
+tError infectiousAgentTable_remove(tInfectiousAgentTable *table, tInfectiousAgent *infectiousAgent) {
+
     // PR1_EX3
+    assert(table != NULL);
+    assert(infectiousAgent != NULL);
 
     int i;
     bool found;
 
-    // To remove an element of a table, we will move all elements after this element one position, to fill the space of the removed element.
+    // It will relocate all items after the one to be removed to one less position
     found = false;
-    for(i=0; i < table->size; i++) {
-        // If the element has been found. Displace this element to the previous element (will never happen for the first one). We use the ADDRESS
-        // of the previous element &(table->elements[i-1]) as destination, and ADDRESS of the current element &(table->elements[i]) as source.
-        if(found) {
-            // Check the return code to detect memory allocation errors
-            if(infectiousAgent_cpy(&(table->elements[i-1]), &(table->elements[i])) == ERR_MEMORY_ERROR) {
-                // Error allocating memory. Just stop the process and return memory error.
+    for (i = 0; i < table->size; i++) {
+        if (found) {
+            // Copy element into its previous position
+            if (infectiousAgent_cpy(&(table->elements[i - 1]), &(table->elements[i])) == ERR_MEMORY_ERROR)
+                // Error allocating or reallocating the memory
                 return ERR_MEMORY_ERROR;
-            }
-        } else if(infectiousAgent_equals(&table->elements[i], infectiousAgent)) {
-            // The current element is the element we want to remove. Set found flag to true to start element movement.
+        } else if (infectiousAgent_equals(&table->elements[i], infectiousAgent)) {
+            // The current element is the element we want to remove. Set found flag to true to start element relocating.
             found = true;
         }
     }
 
-    // Once removed the element, we need to modify the memory used by the table.
-    if(found) {
-        // Modify the number of elements
+    if (found) {
+        // Modify size and used memory for elements
         table->size--;
 
-        // If we are removing the last element, we will assign the pointer to NULL, since we cannot allocate zero bytes
         if (table->size == 0) {
             table->elements = NULL;
         } else {
-            // Modify the used memory. As we are modifying a previously allocated block, we need to use the realloc command.
-            table->elements = (tInfectiousAgent*) realloc(table->elements, table->size * sizeof(tInfectiousAgent));
-
-            // Check that the memory has been allocated
-            if(table->elements == NULL) {
+            table->elements = (tInfectiousAgent *) realloc(table->elements, table->size * sizeof(tInfectiousAgent));
+            if (table->elements == NULL)
                 // Error allocating or reallocating the memory
                 return ERR_MEMORY_ERROR;
-            }
         }
     } else {
-        // If the element was not in the table, return an error.
+        // The element was not found in the table
         return ERR_NOT_FOUND;
     }
 
     return OK;
-
-
-    return ERR_NOT_IMPLEMENTED;
 }
 
 // Get infectious agent by infectious agent name
-tInfectiousAgent* infectiousAgentTable_find(tInfectiousAgentTable* table, const char* infectiousAgentName) {
+tInfectiousAgent *infectiousAgentTable_find(tInfectiousAgentTable *table, const char *infectiousAgentName) {
+
     // PR1_EX3
+    assert(table != NULL);
 
     int i;
 
-    // Search over the table and return once we found the element.
-    for(i=0; i<table->size; i++) {
-        if(strcmp(table->elements[i].name, infectiousAgentName) == 0) {
-            // We return the ADDRESS (&) of the element, which is a pointer to the element
+    for (i = 0; i < table->size; i++) {
+        if (strcmp(table->elements[i].name, infectiousAgentName) == 0) {
             return &(table->elements[i]);
         }
     }
 
-    // The element has not been found. Return NULL (empty pointer).
+    // The element has not been found
     return NULL;
 }
 
 // Get the size of the table
-unsigned int infectiousAgentTable_size(tInfectiousAgentTable* table) {
-	// PR1_EX3
+unsigned int infectiousAgentTable_size(tInfectiousAgentTable *table) {
 
-	return table->size;
+    // PR1_EX3
+    assert(table != NULL);
 
-	return ERR_NOT_IMPLEMENTED;
+    return table->size;
 }
